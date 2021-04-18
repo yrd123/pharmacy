da8755cb8b75dd8c51437ec108bf375946d8922b
passedonwisdom <yash.deorah@somaiya.edu>
2021-04-19 01:49:36 +0530

    ui

--- pharmacy_app/views.py
+++ pharmacy_app/views.py
@@ -12,10 +12,23 @@ def login(request):
     return render(request,'login.html')
 
 def schedule1(request):
-    return render(request,'index.html')
+    return render(request,'schedule1.html')
 
 def schedule2(request):
     return render(request,'index.html')
 
 def schedule3(request):
     return render(request,'index.html')
+
+def registerCustomer(request):
+    return render(request,'registerCustomer.html')
+
+def loginCustomer(request):
+    return render(request,'loginCustomer.html')
+
+def customer(request):
+    return render(request,'customer.html')
+
+
+def billing(request):
+    return render(request,'billing.html')
